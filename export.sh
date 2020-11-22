edaff11fb5dc4fbf060c2bbfdcd8bd151ccc945e
DAC2020-AI <74859173+DAC2020-AI@users.noreply.github.com>
2020-11-22 14:17:11 +0300

    Add files via upload

---
+++ export.sh
@@ -0,0 +1,9 @@
+# remote
+export PYTHONPATH=$PYTHONPATH:/home/muratbuldu/fault-injection/dl_models/
+
+# @muratbuldu
+# export PYTHONPATH=$PYTHONPATH:/Users/muratbuldu/Desktop/ares/dl_models/
+# export PYTHONPATH=$PYTHONPATH:/Users/muratbuldu/Desktop/fault-injection/dl_models/
+
+# cmpe
+# export PYTHONPATH=$PYTHONPATH:/raid/users/ykoroglu/fault-injection/dl_models
